d6f7e9a0bec10a28fa6c3d9ccb6e25bc7cb54b69
LENOVO <LENOVO@LAPTOP-JASH>
2023-04-06 12:38:19 +0530

    Add project files.

---
+++ Model/Employee.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeRequisitionPortal.Model
+{
+    public class Employee:IdentityUser
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
